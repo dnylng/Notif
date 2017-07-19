0a04cfbab00caf4c592d4657fa9f1359aefa31f6
dnylng <iamdannyluong@gmail.com>
2017-07-19 13:39:11 -0400

    Got notification after 5 seconds

--- ViewController.swift
+++ ViewController.swift
@@ -25,5 +25,36 @@ class ViewController: UIViewController {
         })
     }
 
+    @IBAction func notifyBtnPressed(_ sender: Any) {
+        scheduleNotif(inSeconds: 5, completion: { success in
+            if success {
+                print("Successfully scheduled notification")
+            } else {
+                print("Error in scheduling notification")
+            }
+        })
+    }
+    
+    func scheduleNotif(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        let notif = UNMutableNotificationContent()
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These will keep you up to date!"
+        notif.body = "Check out all of our new amazing updates to the app. Please continue to read our notifications."
+        
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print(error!)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        })
+    }
+    
 }
 
